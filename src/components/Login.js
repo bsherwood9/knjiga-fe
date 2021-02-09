171d15737e36f82ce7ede590a507f56d18138d53
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2021-02-08 23:54:22 -0700

    adding formik forms and yup schema validation to login and register. Still need to improve validation schema and flesh it out.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,57 @@
 import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 import axios from "axios";
 
+const loginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid Email").required("Required"),
+  password: Yup.string()
+    .required("Required")
+    .min(8, "Password should be at least 8 characters"),
+});
 function Login() {
-  const [logData, setLogData] = useState({ email: "", password: "" });
-  const handleChange = (e) => {
-    setLogData({ ...logData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      .post("http://localhost:4001/api/auth/login", logData, {
-        withCredentials: true,
-      })
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  };
   return (
     <div className="login-form">
       <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            name="email"
-            type="email"
-            value={logData.email}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            name="password"
-            type="password"
-            value={logData.password}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">Login</button>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => {
+          axios
+            .post("http://localhost:4001/api/auth/login", values, {
+              withCredentials: true,
+            })
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err));
+        }}
+        validationSchema={loginSchema}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <label htmlFor="email">Email</label>
+            <Field
+              name="email"
+              type="email"
+              placeholder="jane@email.com"
+              // onChange={handleChange}
+            />
+            {touched.email && errors.email ? <p>{errors.email}</p> : null}
+
+            <label htmlFor="password">Password</label>
+            <Field
+              name="password"
+              type="password"
+              placeholder="password"
+              // value={logData.password}
+              // onChange={handleChange}
+            />
+            {touched.password && errors.password ? (
+              <p>{errors.password}</p>
+            ) : null}
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,81 +1,87 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const registerSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(20, "Too Long!")
+    .required("Required"),
+  password: Yup.string()
+    .min(8, "Password should be at least 8 characters")
+    .required("Required"),
+  cpassword: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+  email: Yup.string().email("Invalid email").required("Required"),
+});
 
 function Register() {
-  const [formData, setFormData] = useState({
-    name: "",
-    password: "",
-    email: "",
-    cpassword: "",
-  });
-  const [error, setError] = useState(false);
-  const handleChange = (e) => {
-    setError(false);
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-  const handleSubmit = (e) => {
-    setError(false);
-    e.preventDefault();
-    console.log(formData);
-    if (formData.password !== formData.cpassword) {
-      setError("Passwords aren't matching.");
-    } else if (formData.password.length < 8) {
-      setError("Password needs to be at least 8 characters.");
-    } else {
-      let { email, password, name } = formData;
-      axios
-        .post("http://localhost:4001/api/auth/register", {
-          email,
-          password,
-          name,
-        })
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((err) => console.log(err));
-    }
+  const handleSubmit = (values) => {
+    let { email, password, name } = values;
+
+    axios
+      .post("http://localhost:4001/api/auth/register", {
+        email,
+        password,
+        name,
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div className="auth-form">
       <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name
-          <input
-            name="name"
-            type="text"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Email
-          <input
-            name="email"
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </label>
-        {error && <h1>{error}</h1>}
-        <label>
-          Password
-          <input
-            name="password"
-            type="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          s<label htmlFor="cpassword">Confirm Password</label>
-          <input
-            name="cpassword"
-            type="password"
-            value={formData.cpassword}
-            onChange={handleChange}
-          />
-        </label>
-        <button>Submit</button>
-      </form>
+      <Formik
+        initialValues={{
+          password: "",
+          email: "",
+          cpassword: "",
+          name: "",
+        }}
+        onSubmit={(values, { resetForm }) => {
+          handleSubmit(values);
+          resetForm();
+        }}
+        validationSchema={registerSchema}
+      >
+        {({ errors, touched, isSubmitting }) => (
+          <Form>
+            <label htmlFor="name">FullName</label>
+            <Field name="name" type="text" />
+            {touched.name && errors.name ? <p>{errors.name}</p> : null}
+            <label htmlFor="email">Email</label>
+            <Field name="email" type="email" placeholder="jane@email.com" />
+            {touched.email && errors.email ? <p>{errors.email}</p> : null}
+            <label htmlFor="password">Password</label>
+            <Field
+              name="password"
+              type="password"
+              // value={formData.password}
+              // onChange={handleChange}
+            />
+            {touched.password && errors.password ? (
+              <p>{errors.password}</p>
+            ) : null}
+            s<label htmlFor="cpassword">Confirm Password</label>
+            <Field
+              name="cpassword"
+              type="password"
+              // value={formData.cpassword}
+              // onChange={handleChange}
+            />
+            {touched.cpassword && errors.cpassword ? (
+              <p>{errors.cpassword}</p>
+            ) : null}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
